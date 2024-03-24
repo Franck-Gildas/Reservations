36013689f301fca1f996dbd2b57a2973b6278c5b
Franck Gildas <efmukamk01@monccnb.ca>
2024-03-24 11:57:26 -0300

    Add project files.

---
+++ Hotel/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Hotel.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Hotel.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
---
+++ Hotel/Models/Reservation.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Hotel.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? CustomerName { get; set; }
+
+        public int NumberOfDays { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
